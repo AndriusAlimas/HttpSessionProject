7318bce4a6bf4b3cf1b83273e875a933637e577d
Andrius <andriusjavait@gmail.com>
2017-01-21 20:47:04 +0000

    added 3 Servlets with simple blank template code update DD with all servlets mapping

---
+++ HttpSessionProject/src/com/example/servlet/MyServlet1.java
@@ -0,0 +1,10 @@
+package com.example.servlet;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+public class MyServlet1 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, 
+			HttpServletResponse response)throws ServletException,IOException{
+		
+	}
+}
